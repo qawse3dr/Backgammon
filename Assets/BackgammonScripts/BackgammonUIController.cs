b558dded7a53013a503b355705782553e297e7ec
qawse3dr <lawrencemilne38@gmail.com>
2021-10-05 12:30:30 -0400

    Menu: Create Basic Menu #39 (Part 1)
    Created a basic menu that has 2 clickable options (Play, and Quit) that are both funcitonal
    Either quiting the applciation or bringing you to a temp Backgammon scene from the backgammon Scene
    a main Menu option was added just so you weren't stuck in the temp scene
    
    A MouseClickAndHoverText conteroller was added that changes the colour on hover
    and adds onclick to text. This can be applied to any text object to supply an easy onclick
    and should be resued where we can
    
    TODO:
      Unit Testing. This will have to wait till I figure out how to do it....

---
+++ Assets/BackgammonScripts/BackgammonUIController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/** This calls is responsible for all UI elements on the screen
+ * This includes text popups, and so on but not the gameboard itself
+ * that should be handled somewhere else
+ */
+public class BackgammonUIController : MonoBehaviour {
+  public void MainMenuOnClick() {
+    Debug.Log("Exiting Game Going back to MainMenu.");
+
+    /* A warning should probably be added explain it will
+     * not save the current state of the game and all progress will
+     * be lost. Maybe it should also count as a LOSS for both of them
+     * or maybe a draw.
+     */
+
+    SceneManager.LoadScene("MainMenu");
+  }
+}
---
+++ Assets/Common/MouseClickAndHoverText.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+/** This script should be attached to a UI>Text object and will control the behaviours
+ * of it when the mouse hovers over it as well as adds OnClick support
+ * when text is hovered over it will change colour to HoverColour and change back to the
+ * default after the mouse hovers away
+ */
+public class MouseClickAndHoverText : MonoBehaviour,
+                                      IPointerClickHandler,
+                                      IPointerEnterHandler,
+                                      IPointerExitHandler {
+  // OnClick's that will be clicked when the text is clicked
+  public UnityEvent OnClick;
+
+  // Colour for hover (This should be set in the Unity GUI).
+  public Color HoverColour;
+  // Text this script gets attached too
+  private Text _text;
+
+  // The Default color that will be reverted to when the mouse is no longer hovering
+  private Color _defaultColour;
+
+  void Start() {
+    // Try to get text component, if it doesn't exist we should exit with error as this script needs
+    // a text component
+    if (!TryGetComponent<Text>(out _text)) {
+      Debug.LogError("Required Text Object is not found");
+      Application.Quit(1);
+    }
+
+    _defaultColour = _text.color;
+  }
+
+  /** Inherited from IPointerEnterHandler
+   *  This method will be called when the mouse hovers over the text (This should not be called
+   * except for by unity, or unit tests)
+   *  @param pointerEventData
+   */
+  public void OnPointerEnter(PointerEventData pointerEventData) {
+    _text.color = HoverColour;
+  }
+
+  /** Inherited from IPointerExitHandler
+   *  This method will be called when the mouse stops hovering over the text (This should not be
+   * called except for by unity, or unit tests)
+   *  @param pointerEventData
+   */
+  public void OnPointerExit(PointerEventData pointerEventData) {
+    _text.color = _defaultColour;
+  }
+
+  /** Inherited from IPointerClickHandler
+   *  This method will be called when the mouse clicks on the text (This should not be called except
+   * for by unity, or unit tests) Once the click occurs the functions OnClicks will be Invoked.
+   *  @param pointerEventData
+   */
+  public void OnPointerClick(PointerEventData pointerEventData) {
+    OnClick.Invoke();
+  }
+}
