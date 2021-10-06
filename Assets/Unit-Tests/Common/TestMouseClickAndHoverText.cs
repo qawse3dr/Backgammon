9815d5b4db00d9a0cdbcdbcd627871c6eb607936
qawse3dr <lawrencemilne38@gmail.com>
2021-10-06 00:43:13 -0400

    Menu: Create Basic Menu #39 (Part 2) QA: Setup automated unit-test
    Type: Dev Story
    
    This Part Mainly focused with unit tests and getting gitlab to display the
    results.

---
+++ Assets/Unit-Tests/Common/TestMouseClickAndHoverText.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.TestTools;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class TestMouseClickAndHoverText {
+  private Text _textElement;
+  private GameObject _object;
+  private MouseClickAndHoverText _controller;
+
+  [UnitySetUp]
+  public IEnumerator Setup() {
+    SceneManager.LoadScene("MainMenu");
+    yield return new WaitForSeconds(1);
+
+    _object = new GameObject("Text", typeof(Text));
+    _object.AddComponent<MouseClickAndHoverText>();
+    _textElement = _object.GetComponent<Text>();
+    _controller = _object.GetComponent<MouseClickAndHoverText>();
+
+    // Set colours for hover testing.
+    _textElement.color = Color.green;
+    _controller.HoverColour = Color.gray;
+  }
+
+  // This class is used in Test_MouseOnClick as a onClick wrapper.
+  private class TestCallbackMouseClickObj : MonoBehaviour {
+    public bool Clicked = false;
+
+    public void OnClick() {
+      Clicked = true;
+    }
+  }
+
+  // A simple test check if the OnClick Get invoked.
+  // If TestCallbackMouseClickObj.Clicked = true, the test passed.
+  [UnityTest]
+  public IEnumerator Test_MouseOnClick() {
+    // Create class that will be a wrapper for the OnClick
+    TestCallbackMouseClickObj onClick =
+        new GameObject("TestCallbackMouseClickObj", typeof(TestCallbackMouseClickObj))
+            .GetComponent<TestCallbackMouseClickObj>();
+
+    // Create OnClick
+    _controller.OnClick = new UnityEvent();
+    UnityAction unityAction = onClick.OnClick;
+    _controller.OnClick.AddListener(unityAction);
+
+    // Simulate mouse click
+    _controller.OnPointerClick(null);
+
+    // Use the Assert class to test conditions
+    yield return new WaitForSeconds(1);
+
+    Assert.AreEqual(true, onClick.Clicked, "OnClick was never triggered for mouse click.");
+  }
+
+  [UnityTest]
+  public IEnumerator Test_MouseEnterHover() {
+    Assert.AreEqual(Color.green, _textElement.color,
+                    "Default colour was changed. Please change it back to green for testing.");
+
+    // Simulate hover
+    _controller.OnPointerEnter(null);
+    yield return new WaitForSeconds(1);
+    Assert.AreEqual(Color.gray, _textElement.color, "Colour was not changed on Hover");
+  }
+
+  [UnityTest]
+  public IEnumerator Test_MouseExitHover() {
+    // Set default value
+    _controller.OnPointerEnter(null);
+    yield return new WaitForSeconds(1);
+    Assert.AreEqual(Color.gray, _textElement.color,
+                    "Default colour was changed. Please change it back to green for testing.");
+
+    // Simulate hover leaving
+    _controller.OnPointerExit(null);
+    yield return new WaitForSeconds(1);
+
+    Assert.AreEqual(Color.green, _textElement.color, "Colour was not changed back on Hover exit");
+  }
+}
---
+++ Assets/Unit-Tests/MainMenu/TestMainMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+using NUnit.Framework.Internal;
+
+// using MenuSystemController;
+public class TestMainMenu {
+  private MenuSystemController _menuController;
+
+  [UnitySetUp]
+  public IEnumerator Setup() {
+    SceneManager.LoadScene("MainMenu");
+    yield return new WaitForSeconds(1);
+
+    _menuController = new GameObject("MenuSystemController", typeof(MenuSystemController))
+                          .GetComponent<MenuSystemController>();
+  }
+
+  // A Test behaves as an ordinary method
+  [UnityTest]
+  public IEnumerator Test_MainMenuStartGame() {
+    // Invoke click
+    _menuController.StartGameOnClick();
+
+    // For some reason it only works with 2 Waits but this
+    // Wait for the scene to become active
+    yield return new WaitForSeconds(1);
+
+    // Test to see if Scene was changed
+    Assert.AreEqual(SceneManager.GetActiveScene().name,
+                    SceneManager.GetSceneByName("Backgammon").name);
+  }
+
+  /** Application can't actually quit so just make sure this doesn't crash */
+  [Test]
+  public void Test_MainMenuQuitGame() {
+    _menuController.QuitGameOnClick();
+  }
+}
