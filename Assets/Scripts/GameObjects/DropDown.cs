f3ebc836f92420149e0bf5808f228d107a406501
Rachel Broders <rachelbroders@gmail.com>
2021-11-15 13:45:45 -0500

    Initial design for stats page
    Static design of Stats page UI implemented in unity
    
    idk
    
    WIP
    
    broken

---
+++ Assets/Scripts/GameObjects/DropDown.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Logger = LNAR.Logger;
+
+
+public class DropDown : MonoBehaviour
+{
+
+    public void HandleInputData(int val){
+        if(val == 0){
+            Logger.Info($"Val 0");
+        }
+        if(val == 1){
+            Logger.Info($"Val 0");
+        }
+        if(val == 2){
+            Logger.Info($"Val 0");
+        }
+        if(val == 3){
+            Logger.Info($"Val 0");
+        }
+
+
+
+    }
+
+}
