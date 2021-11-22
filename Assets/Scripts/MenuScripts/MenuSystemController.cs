058e1a88fac769c83521663819eb3e0be2ca3a87
unknown <ajit.26@hotmail.com>
2021-11-22 14:08:53 -0500

    WIP

--- Assets/Scripts/MenuScripts/MenuSystemController.cs
+++ Assets/Scripts/MenuScripts/MenuSystemController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using Logger = LNAR.Logger;
 
 /** Menu Class should deal with all the main logic included in the menu
@@ -10,7 +11,12 @@ using Logger = LNAR.Logger;
  *   Changing Scene functionality
  */
 public class MenuSystemController : MonoBehaviour {
+
+  public GameObject CreatePlayerPopup;
+  public InputField input;
+
   void Start() {
+    CreatePlayerPopup.SetActive(false);
     Database.DB_PATH = "stats.db";
   }
   public void StartGameOnClick() {
@@ -25,10 +31,28 @@ public class MenuSystemController : MonoBehaviour {
 
   public void EditProfilesOnClick() {
     Logger.Info("Opening Edit Profiles Page...");
+    Logger.Info("Creating Profile...");
+    CreatePlayerPopup.SetActive(true);
   }
 
   public void StatsPageOnClick() {
     Logger.Info("Redirecting to stats page...");
     SceneManager.LoadScene("StatsScene");
+  }
+
+  public void MainMenuOnClick() {
+    Logger.Info("Going to Main Menu...");
+    CreatePlayerPopup.SetActive(false);
+  }
+
+  public void PlayerCreation() {
+    //var input = gameObject.GetComponent<InputField>();
+    Logger.Info(input.text);
+
+    if(input.text == ""){
+      GameObject.Find("CreatePlayerHeader").GetComponent<Text>().text = "Please Do Not Leave The Text Field Empty";
+    } else {
+      CreatePlayerPopup.SetActive(false);
+    }
   }
 }
